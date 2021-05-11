5ec86bcff8de811e56dc72a9df25f61d178a739b
Jon <jbrundagejr@gmail.com>
2021-05-11 14:09:44 -0400

    comics on page

--- src/components/ComicPage.js
+++ src/components/ComicPage.js
@@ -1,10 +1,23 @@
 import ComicCard from './ComicCard'
+import {useEffect, useState} from 'react'
 
 function ComicPage(){
+  const [comics, setComics] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/comics')
+      .then(res => res.json())
+      .then(comicData => setComics(comicData))
+  }, [])
+
+  const comic = comics.map(comicObj => {
+    return <ComicCard comic={comicObj} key={comicObj.id}/>
+  })
+
   return (
     <div>
       <h2>Comics!</h2>
-      <ComicCard />
+        {comic}
     </div>
   )
 }
