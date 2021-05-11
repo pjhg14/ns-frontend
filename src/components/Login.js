5de86b7e5a0a00658596f71d5a27499720b4b5e9
pj <pjhg14@gmail.com>
2021-05-11 14:50:48 -0400

    before merge

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
+import { Button, Form, Message } from 'semantic-ui-react'
+
 function Login(){
   return(
     <div>
       <h1>Login</h1>
+      <Form error>
+        <Form.Input label='Email' placeholder='joe@schmoe.com' />
+        <Message
+          error
+          header='Action Forbidden'
+          content='You can only sign up for an account once with a given e-mail address.'
+        />
+        <Button>Submit</Button>
+      </Form>
     </div>
   )
 }
